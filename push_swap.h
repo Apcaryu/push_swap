3bef26a302719cea06a3e885c70b7cc2befc653d
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-10-11 17:09:37 +0200

    add struct for a and b

--- push_swap.h
+++ push_swap.h
@@ -11,6 +11,13 @@
 # include "test.h"
 // ----------------------
 
+typedef struct	s_tab_ab
+{
+	long int		*tab;
+	unsigned int	size;
+	unsigned int	top_a;
+}	t_tab_ab;
+
 void	is_full_numbers(char *arg);
 int		*init_tab(char *list);
 
