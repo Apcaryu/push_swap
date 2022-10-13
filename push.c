eb53dcd7111d614cb92f665215521b50d90d50e9
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-10-13 17:13:50 +0200

    add push function

--- push.c
+++ push.c
@@ -1,4 +1,9 @@
 #include "instruction_set.h"
 
 void	push(t_tab_ab *tabs, char target)
-{}
+{
+	if (target == 'a' && -1 < tabs->top_a)
+		tabs->top_a -= 1;
+	else if (target == 'b' && tabs->top_a < tabs->size - 1)
+		tabs->top_a += 1;
+}
