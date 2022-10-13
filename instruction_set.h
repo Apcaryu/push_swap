1eeab7fd352b4426bbdbaacc962e63f3c3dcfc36
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-10-13 15:41:23 +0200

    add push.c

--- instruction_set.h
+++ instruction_set.h
@@ -13,5 +13,6 @@ typedef struct	s_tab_ab
 
 void	swap(int *tab, char heap);
 void	new_swap(t_tab_ab *tabs, char target);
+void	push(t_tab_ab *tabs, char target);
 
 #endif
---
+++ push.c
@@ -0,0 +1,4 @@
+#include "instruction_set.h"
+
+void	push(t_tab_ab *tabs, char target)
+{}
