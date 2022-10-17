bf05830f659c8209f2c644d739df66f2c3add163
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-10-17 15:15:56 +0200

    add set_bottom_top function

--- rotate.c
+++ rotate.c
@@ -1,5 +1,19 @@
 #include "instruction_set.h"
 
+static void	set_bottom_top(t_tab_ab tabs, unsigned int *bottom, unsigned int *top, char target)
+{
+	if (target == 'a')
+	{
+		*bottom = 0;
+		*top = tabs.top_a;
+	}
+	else if (target == 'b')
+	{
+		*bottom = tabs.size - 1;
+		*top = tabs.top_a + 1;
+	}
+}
+
 void	rotate(t_tab_ab *tabs, char target, char is_reverse)
 {
 	int	tmp;
