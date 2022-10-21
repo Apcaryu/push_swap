20047e2b32d42cfc0e7545e292f699c3f54cd220
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-10-21 17:17:47 +0200

    add command output for pa and pb

--- push.c
+++ push.c
@@ -3,7 +3,13 @@
 void	push(t_tab_ab *tabs, char target)
 {
 	if (target == 'a' && tabs->top_a < tabs->size - 1)
+	{
 		tabs->top_a += 1;
+		write(1, "pa\n", 3);
+	}
 	else if (target == 'b' && 0xFFFFFFFF != tabs->top_a)
+	{
 		tabs->top_a -= 1;
+		write(1, "pb\n", 3);
+	}
 }
