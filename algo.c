653f08540f0066b60c53b17a9539c83757d89c18
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-11-07 16:17:36 +0100

    add while for ordered the list

--- algo.c
+++ algo.c
@@ -5,14 +5,21 @@ char	check_order(t_tab_ab tabs)
 {
 	unsigned int	idx;
 
-	idx = tabs.top_a - 1;
+	idx = tabs.top_a;
 	while (0 < idx)
 	{
-		if (tabs.tab[idx] < tabs.tab[idx + 1])
+		if (tabs.tab[idx - 1] < tabs.tab[idx])
 			return (0);
 		idx--;
 	}
 	return (1);
+}
+
+char	check_minmax(t_tab_ab tabs, int min, int max)
+{
+	if (tabs.tab[tabs.top_a - 1] == min && tabs.tab[tabs.top_a] == max)
+		return (1);
+	return (0);
 }
 
 int	get_max(t_tab_ab tabs)
@@ -49,13 +56,22 @@ int	get_min(t_tab_ab tabs)
 
 void	move_or_not(t_tab_ab *tabs)
 {
-	char do_rotate;
+	char	do_rotate;
+	int		min;
+	int		max;
 
 	do_rotate = 0;
 
-	if (tabs->tab[tabs->top_a - 1] < tabs->tab[tabs->top_a] && !check_order(*tabs))
-		swap(tabs, 'a');
-	if (!check_order(*tabs))
-		rotate(tabs, 'a', 0);
+	min = get_min(*tabs);
+	max = get_max(*tabs);
+	while (!check_order(*tabs))
+	{
+		if (check_minmax(*tabs, min, max))
+			rotate(tabs, 'a', 0);
+		if (tabs->tab[tabs->top_a - 1] < tabs->tab[tabs->top_a] && !check_order(*tabs))
+			swap(tabs, 'a');
+		if (!check_order(*tabs))
+			rotate(tabs, 'a', 0);
+	}
 	new_print_tab(*tabs, 'a');
 }
