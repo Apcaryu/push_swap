7b51d2cd5d303260ae75f9081ce3eaecbd75b565
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-11-25 18:07:04 +0100

    add sequence_size function

--- better_sequence.c
+++ better_sequence.c
@@ -10,6 +10,34 @@
  *         passer au nombre suivant
  *---------------------------------------------------------------------------*/
 
+void	sequence_size(t_tab_ab data_tabs, t_data_sequence *data_seq, unsigned int start)
+{
+	unsigned int	idx;
+	unsigned int	size;
+	int 			tmp;
+	
+	idx = start - 1;
+	size = 1;
+	if (idx == 4294967295)
+	    idx = data_tabs.top_a;
+	tmp = data_tabs.tab[idx];
+	idx--;
+	while (idx != start)
+	{
+		if (idx == 4294967295)
+			idx = data_tabs.top_a;
+		if (tmp < data_tabs.tab[idx])
+			size++;
+		idx--;
+	}
+	if (data_seq->seq_size < size)
+	{
+		data_seq->seq_size = size;
+		data_seq->idx_better_nb = start;
+	}
+}
+
 void	better_sequence(t_tab_ab *data_tabs)
 {
+	t_data_sequence data_seq;
 }
