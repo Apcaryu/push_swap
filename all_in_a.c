23cfa67ccb0e3ee1da7ec29c3533055822001e4e
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-05 17:46:10 +0100

    use check_push_in_a function

--- all_in_a.c
+++ all_in_a.c
@@ -57,7 +57,14 @@ void	all_in_a(t_tab_ab *data_tabs)
 	mm_ab.max_b = INT_MIN;
 	set_min_max(*data_tabs, &mm_ab);
 	printf("ma = %d Ma = %d | mb = %d Mb = %d\n", mm_ab.min_a, mm_ab.max_a, mm_ab.min_b, mm_ab.max_b);
-	printf("push? %d\n", check_push_in_a(*data_tabs, &mm_ab));
+//	printf("push? %d\n", check_push_in_a(*data_tabs, &mm_ab));
+	while (data_tabs->top_a < data_tabs->size - 1)
+	{
+		if (check_push_in_a(*data_tabs, &mm_ab))
+			push(data_tabs, 'a');
+		else
+			rotate(data_tabs, 'a', 0);
+	}
 //	while (data_tabs->tab[data_tabs->top_a + 1] != data_tabs->max)
 //		rotate(data_tabs, 'b', 0);
 //	while (data_tabs->top_a < data_tabs->size - 1 || data_tabs->top_a == UINT_MAX)
