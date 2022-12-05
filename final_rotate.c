7b27718d6207ba8a7a617f39f8c658fbc8a858b3
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-05 18:03:35 +0100

    add is_in_top function

--- final_rotate.c
+++ final_rotate.c
@@ -1,5 +1,23 @@
 #include "push_swap.h"
 
+t_bool is_in_top(t_tab_ab data_tabs)
+{
+	unsigned int	idx;
+
+	idx = 0;
+	while (idx < data_tabs.size)
+	{
+		if (data_tabs.tab[idx] == data_tabs.min) {
+			if (idx < data_tabs.size / 2)
+				return (true);
+			else
+				return (false);
+		}
+		idx++;
+	}
+	return (false);
+}
+
 void	final_rotate(t_tab_ab *data_tabs)
 {
 
