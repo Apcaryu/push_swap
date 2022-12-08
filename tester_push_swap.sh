7c10fa69ffc1395d38027a2516db6c4be8368be6
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-08 17:00:03 +0100

    add tester_push_swap.sh and add first test

---
+++ tester_push_swap.sh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+echo "Please select mod:"
+echo "0 : all test"
+echo "1 : 3 numbers"
+echo "2 : 5 numbers"
+echo "3 : 100 numbers"
+echo "4 : 500 numbers"
+echo "5 : quit"
+
+read test_mod
+
+if [[ $test_mod == 1 ]] || [[ $test_mod == 0 ]]
+then
+  echo "\033[33m3 numbers test\033[0m"
+  ARG="3 2 1"
+  nb_moves=$(./t_push_swap $ARG | wc -l);
+  if [[ 3 < $(($nb_moves)) ]]
+  then
+    echo "\033[31mToo many moves. | moves = $nb_moves\033[0m"
+  else
+    echo "\033[32mGood moves numbers | moves = $nb_moves\033[0m"
+  fi
+  is_sort=$(./t_push_swap $ARG | ./checker_linux $ARG)
+  if [[ 'OK' == $is_sort ]]
+  then
+    echo "\033[32mList sorted! 👍\033[0m"
+  else
+    echo "\033[31mNot sorted\033[0m"
+  fi
+fi
+if [[ $test_mod == 2 ]] || [[ $test_mod == 0 ]]
+then
+  echo "5 numbers test"
+fi
+if [[ $test_mod == 3 ]] || [[ $test_mod == 0 ]]
+then
+  echo "100 numbers test"
+fi
+if [[ $test_mod == 4 ]] || [[ $test_mod == 0 ]]
+then
+  echo "500 numbers test"
+fi
+if [[ $test_mod == 5 ]]
+then
+  echo "Goodbye"
+fi
