66742ea7916be4d3b3465392db83f35fc06b8f41
Apcaryu <antoinepellegrin@outlook.fr>
2022-12-12 22:35:46 +0100

    clean swap.c

--- swap.c
+++ swap.c
@@ -21,7 +21,6 @@ void	swap(t_tab_ab *tabs, char target)
 			tmp = tabs->tab[tabs->top_a];
 			tabs->tab[tabs->top_a] = tabs->tab[tabs->top_a - 1];
 			tabs->tab[tabs->top_a - 1] = tmp;
-			// write(1, "sa\n", 3);
 		}
 	}
 	if (target == 'b' || target == 's')
@@ -31,15 +30,7 @@ void	swap(t_tab_ab *tabs, char target)
 			tmp = tabs->tab[tabs->top_a + 1];
 			tabs->tab[tabs->top_a + 1] = tabs->tab[tabs->top_a + 2];
 			tabs->tab[tabs->top_a + 2] = tmp;
-			// write(1, "sb\n", 3);
 		}
 	}
 	swap_printer(target);
-}
-
-void	super_swap(t_tab_ab *tabs)
-{
-	swap(tabs, 's');
-	// swap(tabs, 'a');
-	// swap(tabs, 'b');
 }
