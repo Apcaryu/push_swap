450f62552a710d42ef9548ff51a25938ecf0b0b6
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-13 15:55:56 +0100

    special_three function completed

--- special_three.c
+++ special_three.c
@@ -16,5 +16,14 @@ unsigned int	get_max_pos(t_tab_ab data_tabs)
 
 void	special_three(t_tab_ab *data_tabs)
 {
-	printf("idx = %u", get_max_pos(*data_tabs)); // TODO remove
+//	printf("idx = %u", get_max_pos(*data_tabs)); // TODO remove
+	while (!is_sorted(*data_tabs))
+	{
+		if (get_max_pos(*data_tabs) == 2)
+			rotate(data_tabs, 'a', false);
+		else if (get_max_pos(*data_tabs) == 1)
+			rotate(data_tabs, 'a', true);
+		else if (get_max_pos(*data_tabs) == 0)
+			swap(data_tabs, 'a');
+	}
 }
