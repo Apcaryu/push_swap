b7a59f9add80f94481a03c77070b879f3097468f
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-16 17:55:41 +0100

    norm instruction_set.h

--- instruction_set.h
+++ instruction_set.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instruction_set.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/16 17:51:43 by apellegr          #+#    #+#             */
+/*   Updated: 2022/12/16 17:51:44 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INSTRUCTION_SET_H
 # define INSTRUCTION_SET_H
 # include <stdlib.h>
@@ -6,16 +18,16 @@
 # include "libft/libft.h"
 # include "utils.h"
 
-typedef struct	s_tab_ab
+typedef struct s_tab_ab
 {
 	int				*tab;
 	unsigned int	size;
 	unsigned int	top_a;
 	int				min;
-	int 			max;
+	int				max;
 }	t_tab_ab;
 
-typedef struct	s_data_dist
+typedef struct s_data_dist
 {
 	unsigned int	dist;
 	unsigned int	dist_a;
@@ -23,10 +35,9 @@ typedef struct	s_data_dist
 	unsigned int	nb_rr_rrr;
 	unsigned int	idx;
 	int				num;
-    t_bool          reverse_a;
-    t_bool          reverse_b;
+	t_bool			reverse_a;
+	t_bool			reverse_b;
 }	t_data_dist;
-
 
 void	swap(t_tab_ab *tabs, char target);
 void	push(t_tab_ab *tabs, char target);
