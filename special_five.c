4359ddb55211b7c06db6862d87e3a2942bc1b61c
Apcaryu <antoinepellegrin@outlook.fr>
2022-12-17 00:20:00 +0100

    norm special_five.c

--- special_five.c
+++ special_five.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   special_five.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 00:16:38 by apellegr          #+#    #+#             */
+/*   Updated: 2022/12/17 00:17:33 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 void	special_five(t_tab_ab *data_tabs)
 {
-	t_min_max_ab mm_ab;
+	t_min_max_ab	mm_ab;
 
 	init_min_max_ab(&mm_ab);
 	if (is_sorted(*data_tabs))
-		return;
+		return ;
 	push(data_tabs, 'b');
 	push(data_tabs, 'b');
 	special_three(data_tabs);
