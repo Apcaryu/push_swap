4e29571252afa3fbf28edace0d75672779a68b24
Apcaryu <antoinepellegrin@outlook.fr>
2022-12-17 14:20:51 +0100

    norm push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -37,30 +37,31 @@ typedef struct s_min_max_ab
 	int	max_b;
 }	t_min_max_ab;
 
-int		is_full_numbers(char *arg);
-int		is_full_space(char *arg);
-int		is_double(t_tab_ab data_tab);
-void	init_data_dist(t_data_dist *data_dist);
-void	init_min_max_ab(t_min_max_ab *mm_ab);
-void	set_min_max(t_tab_ab data_tabs, t_min_max_ab *mm_ab);
-void	new_init_tab(t_tab_ab *tabs, char *argv);
-void	better_sequence(t_tab_ab *data_tabs);
-void	distance(t_tab_ab data_tabs, t_data_dist *data_dist, \
+int				is_full_numbers(char *arg);
+int				is_full_space(char *arg);
+int				is_double(t_tab_ab data_tab);
+void			init_data_dist(t_data_dist *data_dist);
+void			init_min_max_ab(t_min_max_ab *mm_ab);
+void			set_min_max(t_tab_ab data_tabs, t_min_max_ab *mm_ab);
+void			new_init_tab(t_tab_ab *tabs, char *argv);
+void			better_sequence(t_tab_ab *data_tabs);
+void			distance(t_tab_ab data_tabs, t_data_dist *data_dist, \
 					t_min_max_ab mm_ab);
-void	all_in_a(t_tab_ab *data_tabs);
-void	final_rotate(t_tab_ab *data_tabs);
-void	special_three(t_tab_ab *data_tabs);
-void	special_five(t_tab_ab *data_tabs);
-t_bool	is_sorted(t_tab_ab data_tabs);
-t_bool	is_not_int(char *c_nb, int i_nb);
-t_bool	check_push_in_a(t_tab_ab data_tabs, t_min_max_ab *mm_ab);
-void	init_data_sequence(t_data_sequence *data_seq, t_tab_ab data_tabs);
-void	copy_tab(t_data_sequence *data_seq);
-void	get_max(t_tab_ab *tabs);
-void	get_min(t_tab_ab *tabs);
-void	freezer(char **tab);
-t_bool	is_top_a(t_tab_ab data_tabs, unsigned int idx);
-t_bool	is_top_b(t_tab_ab data_tabs, unsigned int idx);
+void			all_in_a(t_tab_ab *data_tabs);
+void			final_rotate(t_tab_ab *data_tabs);
+void			special_three(t_tab_ab *data_tabs);
+void			special_five(t_tab_ab *data_tabs);
+t_bool			is_sorted(t_tab_ab data_tabs);
+t_bool			is_not_int(char *c_nb, int i_nb);
+t_bool			check_push_in_a(t_tab_ab data_tabs, t_min_max_ab *mm_ab);
+void			init_data_sequence(t_data_sequence *data_seq, \
+									t_tab_ab data_tabs);
+void			copy_tab(t_data_sequence *data_seq);
+void			get_max(t_tab_ab *tabs);
+void			get_min(t_tab_ab *tabs);
+void			freezer(char **tab);
+t_bool			is_top_a(t_tab_ab data_tabs, unsigned int idx);
+t_bool			is_top_b(t_tab_ab data_tabs, unsigned int idx);
 unsigned int	set_dist_a(t_tab_ab data_tab, t_bool reverse_a, \
 							unsigned int idx);
 unsigned int	set_dist_b(t_tab_ab data_tab, t_bool reverse_b, \
